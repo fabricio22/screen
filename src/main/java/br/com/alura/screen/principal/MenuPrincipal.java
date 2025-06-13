5682eac035291ad79bb520ea8d2898f65c2a251a
brmou556892 <brmou556892@ambev.com.br>
2025-06-13 17:34:24 -0300

    Novo modelo de episódio e streams de filmes

--- src/main/java/br/com/alura/screen/principal/MenuPrincipal.java
+++ src/main/java/br/com/alura/screen/principal/MenuPrincipal.java
@@ -1,52 +1,80 @@
 package br.com.alura.screen.principal;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import br.com.alura.screen.model.DadosEpisodio;
 import br.com.alura.screen.model.DadosSerie;
 import br.com.alura.screen.model.DadosTemporada;
+import br.com.alura.screen.model.Episodio;
 import br.com.alura.screen.services.ConsumoAPI;
 import br.com.alura.screen.services.ConverterDados;
 
 public class MenuPrincipal {
-	
+
 	private Scanner scanner = new Scanner(System.in);
 	private final String ENDERECO = "https://www.omdbapi.com/?t=";
 	private final String API_KEY = "&apikey=6585022c";
 	private ConsumoAPI consumoApi = new ConsumoAPI();
 	private ConverterDados conversor = new ConverterDados();
 	List<DadosTemporada> temporadas = new ArrayList<>();
-	
-	
+
 	public void exibirMenu() {
 		System.out.println("Digite o nome da série para busca:");
 		var nomeSerie = scanner.nextLine();
 		var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
-		
-		
-		for(int i = 1; i <= dados.totalTemporada(); i++) {
+
+		for (int i = 1; i <= dados.totalTemporada(); i++) {
 			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
-		
+
 		temporadas.forEach(System.out::println);
-		
-//		for(int i = 0; i < dados.totalTemporada(); i++) {
-//			List<DadosEpisodio> episodiosTemporadas = temporadas.get(i).episodios();
-//			for(int j = 0; j < episodiosTemporadas.size(); j++) {
-//				System.out.println(episodiosTemporadas.get(j).titulo());
-//			}
-//		}
-//		
+
 		temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+		List<DadosEpisodio> dadosEpisodios = temporadas.stream().flatMap(t -> t.episodios().stream())
+				.collect(Collectors.toList());
+
+		System.out.println("Lista de episodios de todas as temporadas.");
+		System.out.println(dadosEpisodios);
+
+		System.out.println("Os 5 episodios mais bem avaliado da temporada");
+		dadosEpisodios.stream().filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+				.sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed()).limit(5)
+				.forEach(System.out::println);
+
+		List<Episodio> episodios = temporadas.stream()
+				.flatMap(t -> t.episodios().stream().map(e -> new Episodio(t.numero(), e)))
+				.collect(Collectors.toList());
 		
+		episodios.forEach(System.out::println);
 		
+		System.out.println("A partir de que ano você deseja ver os episódios?");
+		var ano = scanner.nextInt();
+		scanner.nextLine();
 		
+		LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+		DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		episodios.stream()
+		.filter(f -> f.getDataLancameto() != null && f.getDataLancameto().isAfter(dataBusca))
+		.forEach(e -> System.out.println(
+				"Temporada: " + e.getTemporada() +
+				  " Episódio: " + e.getTitulo() + 
+				    " Data Lançamento: " + e.getDataLancameto().format(formatador)
+				));
+		
+
 	}
 
 }
